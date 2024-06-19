d9dd7c6708456250be00bb978bc239d086cb0edd
Jun0908 <31527310+Jun0908@users.noreply.github.com>
2024-06-19 15:14:56 +0900

    Update IPFS_NFT.sol

--- contracts/IPFS_NFT.sol
+++ contracts/IPFS_NFT.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract myNFT is ERC721, Ownable {
+    // Mapping from token ID to IPFS CID
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("myNFT", "NFT") {}
+
+    // Function to mint a new NFT with a specified IPFS CID
+    function safeMint(address to, uint256 tokenId, string memory cid) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, cid);
+    }
+
+    // Internal function to set the token URI for a given token
+    function _setTokenURI(uint256 tokenId, string memory cid) internal {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = cid;
+    }
+
+    // Override the tokenURI function to return the IPFS CID
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory cid = _tokenURIs[tokenId];
+        return bytes(cid).length > 0 ? string(abi.encodePacked("ipfs://", cid)) : "";
+    }
+}
