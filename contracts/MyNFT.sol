7e812168f034f9f2c531b6ae04cae8ed22fca462
Jun0908 <31527310+Jun0908@users.noreply.github.com>
2024-06-19 21:05:36 +0900

    Update MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract myNFT is ERC721, Ownable {
-    constructor() ERC721("myNFT", "NFT") {}
+    constructor() ERC721("myNFT", "NFT") Ownable(msg.sender) {}
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
