6caf47f9e547d9164b11e4c583d2b0bb0f9699bd
Jun0908 <31527310+Jun0908@users.noreply.github.com>
2024-06-19 15:07:27 +0900

    Update ERC6551Account.sol

--- contracts/ERC6551Account.sol
+++ contracts/ERC6551Account.sol
@@ -3,19 +3,29 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "./interfaces/IERC6551Account.sol";
-
 import "./lib/MinimalReceiver.sol";
 import "./lib/ERC6551AccountLib.sol";
 
 contract ERC6551Account is IERC165, IERC1271, IERC6551Account {
     uint256 public nonce;
+
+    // 複数のNFT情報を格納する変数
+    address[] public tokenContracts;
+    uint256[] public tokenIds;
+    uint256[] public chainIds;
+
+    // コンストラクタを追加
+    constructor(address[] memory _tokenContracts, uint256[] memory _tokenIds, uint256[] memory _chainIds) {
+        require(_tokenContracts.length == _tokenIds.length && _tokenIds.length == _chainIds.length, "Input arrays must have the same length");
+        tokenContracts = _tokenContracts;
+        tokenIds = _tokenIds;
+        chainIds = _chainIds;
+    }
 
     receive() external payable {}
 
@@ -24,7 +34,7 @@ contract ERC6551Account is IERC165, IERC1271, IERC6551Account {
         uint256 value,
         bytes calldata data
     ) external payable returns (bytes memory result) {
-        require(msg.sender == owner(), "Not token owner");
+        require(isOwner(msg.sender), "Not token owner");
 
         ++nonce;
 
@@ -40,23 +50,28 @@ contract ERC6551Account is IERC165, IERC1271, IERC6551Account {
         }
     }
 
-    function token()
+    function tokens()
         external
         view
         returns (
-            uint256,
-            address,
-            uint256
+            uint256[] memory,
+            address[] memory,
+            uint256[] memory
         )
     {
-        return ERC6551AccountLib.token();
+        return (chainIds, tokenContracts, tokenIds);
     }
 
-    function owner() public view returns (address) {
-        (uint256 chainId, address tokenContract, uint256 tokenId) = this.token();
-        if (chainId != block.chainid) return address(0);
-
-        return IERC721(tokenContract).ownerOf(tokenId);
+    function owners() public view returns (address[] memory) {
+        address[] memory ownersList = new address[](tokenContracts.length);
+        for (uint256 i = 0; i < tokenContracts.length; i++) {
+            if (chainIds[i] != block.chainid) {
+                ownersList[i] = address(0);
+            } else {
+                ownersList[i] = IERC721(tokenContracts[i]).ownerOf(tokenIds[i]);
+            }
+        }
+        return ownersList;
     }
 
     function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
@@ -69,12 +84,23 @@ contract ERC6551Account is IERC165, IERC1271, IERC6551Account {
         view
         returns (bytes4 magicValue)
     {
-        bool isValid = SignatureChecker.isValidSignatureNow(owner(), hash, signature);
-
-        if (isValid) {
-            return IERC1271.isValidSignature.selector;
+        address[] memory ownersList = owners();
+        for (uint256 i = 0; i < ownersList.length; i++) {
+            if (SignatureChecker.isValidSignatureNow(ownersList[i], hash, signature)) {
+                return IERC1271.isValidSignature.selector;
+            }
         }
 
         return "";
+    }
+
+    function isOwner(address addr) internal view returns (bool) {
+        address[] memory ownersList = owners();
+        for (uint256 i = 0; i < ownersList.length; i++) {
+            if (ownersList[i] == addr) {
+                return true;
+            }
+        }
+        return false;
     }
 }
